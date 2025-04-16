7792f7ffe3dfbda1dd314bc2b1c0d5850f5573ca
Suraj Thapa <suraj.thapa@docsumo.com>
2025-04-16 20:52:48 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import {Button} from "antd";
+
+function App() {
+  return (
+   <main>
+        <Button>Example button from antd </Button>
+   </main>
+  );
+}
+
+export default App;
